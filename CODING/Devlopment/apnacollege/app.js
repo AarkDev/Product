b1f36194414896b94a5af43697b93cb4a0fab3d5
Utkarsh Sahu <1969boogeymen@gmail.com>
2024-09-20 01:50:01 +0530

    Delete CODING/Devlopment/apnacollege directory

--- CODING/Devlopment/apnacollege/app.js
+++
@@ -1,8 +0,0 @@
-let smallImages =document.getElementsByClassName("oldImg");
-console.log(smallImages);
-console.dir(smallImages);
-for(let i=0;i<smallImages.length;i++){
-    smallImages[i].src = " apnaimage/garbage/spiderman_img (1).png";
-    console.dir(smallImages[i].src);
-    console.log(smallImages[i].src);
-}
